b98afbcaa639fb09514b56607ad731c391ea54a6
ANDREW FINDLAY <AF187249@falmouth.ac.uk>
2018-01-08 15:40:48 +0000

    Added comments to camera functions

--- COMP220/COMP220_Examples/OpenGLProject/Camera.h
+++ COMP220/COMP220_Examples/OpenGLProject/Camera.h
@@ -17,12 +17,19 @@ public:
 	class Camera();
 	~Camera();
 
+	// Moves camera based on mouse movement
 	void MoveView(float windowWidth, float windowHeight);
+	// Moves camera down by an increasing amount of not on the ground
 	void ApplyGravity(float groundHeight);
+	// Moves the camera up over time
 	void Jump();
+	// Moves the camera up and down
 	void Fly(float magnitude);
+	// Moves the camera the way it's facing
 	void Forward(float magnitude);
+	// Moves the camera 90 degrees to the way it's facing
 	void Sideways(float magnitude);
+	// Increases or decreases the amount the mouse moves the camera
 	void AdjustMouseSensitivity(float magnitude);
 	
 	glm::vec3 cameraPosition;
